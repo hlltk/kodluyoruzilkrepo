1c61568510522d93cd0debc3f20aed66901e9856
hlltk <hlltkhll@gmail.com>
2022-10-13 20:19:55 +0200

    free projects

--- Free Ptojects/color flipper/app.js
+++ Free Ptojects/color flipper/app.js
@@ -9,5 +9,5 @@ btn.addEventListener("click", function () {
 })
 
 function getRandomNumber() {
-    return Math.floor(Math.random()*colors.length) 
+    return Math.floor(Math.random() * colors.length)
 }
